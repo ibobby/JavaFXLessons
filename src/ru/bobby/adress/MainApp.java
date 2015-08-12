56d35f30effe718f36afe251d906e9ceff3b8175
b.istomin <ibobby0386@gmail.com>
2015-08-12 22:35:48 +0300

    some trouble with linking between PersonOverviewController.java and fxml

--- src/ru/bobby/adress/MainApp.java
+++ src/ru/bobby/adress/MainApp.java
@@ -1,12 +1,17 @@
 package ru.bobby.adress;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import ru.bobby.adress.model.Person;
+import ru.bobby.adress.view.PersonOverviewController;
+
 import java.io.IOException;
 
 /**
@@ -16,6 +21,20 @@ public class MainApp extends Application {
 
     private Stage primaryStage;
     private BorderPane rootLayout;
+
+    private ObservableList<Person> personData = FXCollections.observableArrayList();
+
+    public MainApp() {
+        personData.add(new Person("bob", "best"));
+        personData.add(new Person("pete", "joch"));
+        personData.add(new Person("pete", "tong"));
+        personData.add(new Person("mike", "erman"));
+        personData.add(new Person("paul", "vand"));
+    }
+
+    public ObservableList<Person> getPersonData() {
+        return personData;
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -53,6 +72,9 @@ public class MainApp extends Application {
             AnchorPane personOverview = (AnchorPane) loader.load();
 
             rootLayout.setCenter(personOverview);
+
+            PersonOverviewController controller = loader.getController();
+            controller.setMainApp(this);
 
         } catch (IOException e) {
             e.printStackTrace();
--- src/ru/bobby/adress/model/Person.java
+++ src/ru/bobby/adress/model/Person.java
@@ -29,4 +29,77 @@ public class Person {
         this.city = new SimpleStringProperty("some city");
         this.birthday = new SimpleObjectProperty<LocalDate>(LocalDate.of(1999, 2, 1));
     }
+
+
+    public String getFirstName() {
+        return firstName.get();
+    }
+
+    public StringProperty firstNameProperty() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName.set(firstName);
+    }
+
+    public String getLastName() {
+        return lastName.get();
+    }
+
+    public StringProperty lastNameProperty() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName.set(lastName);
+    }
+
+    public String getStreet() {
+        return street.get();
+    }
+
+    public StringProperty streetProperty() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street.set(street);
+    }
+
+    public int getPostalCode() {
+        return postalCode.get();
+    }
+
+    public IntegerProperty postalCodeProperty() {
+        return postalCode;
+    }
+
+    public void setPostalCode(int postalCode) {
+        this.postalCode.set(postalCode);
+    }
+
+    public String getCity() {
+        return city.get();
+    }
+
+    public StringProperty cityProperty() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city.set(city);
+    }
+
+    public LocalDate getBirthday() {
+        return birthday.get();
+    }
+
+    public ObjectProperty<LocalDate> birthdayProperty() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday.set(birthday);
+    }
 }
