07048ed2bbb53a8e0c245b541b19c93651390949
b.istomin <ibobby0386@gmail.com>
2015-08-14 18:03:51 +0300

    add person edit form

--- src/ru/bobby/adress/view/PersonEditDialogController.java
+++ src/ru/bobby/adress/view/PersonEditDialogController.java
@@ -3,7 +3,10 @@ package ru.bobby.adress.view;
 import javafx.fxml.FXML;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.controlsfx.dialog.Dialog;
+import org.controlsfx.dialog.Dialogs;
 import ru.bobby.adress.model.Person;
+import ru.bobby.adress.util.DateUtil;
 
 /**
  * Created by User on 13.08.2015.
@@ -40,5 +43,86 @@ public class PersonEditDialogController {
         this.dialogStage = dialogStage;
     }
 
+    public void setPerson(Person person) {
+        this.person = person;
+
+        firstNameField.setText(person.getFirstName());
+        lastNameField.setText(person.getLastName());
+        streetField.setText(person.getStreet());
+        postalCodeField.setText(Integer.toString(person.getPostalCode()));
+        cityField.setText(person.getCity());
+        birthdayField.setText(DateUtil.format(person.getBirthday()));
+    }
+
+    public boolean isOkClicked() {
+        return okClicked;
+    }
+
+    @FXML
+    private void handleOk() {
+        person.setFirstName(firstNameField.getText());
+        person.setLastName(lastNameField.getText());
+        person.setStreet(streetField.getText());
+        person.setPostalCode(Integer.parseInt(postalCodeField.getText()));
+        person.setCity(cityField.getText());
+        person.setBirthday(DateUtil.parse(birthdayField.getText()));
+
+        okClicked = true;
+        dialogStage.close();
+    }
+
+    @FXML
+    private void handleCancel() {
+        dialogStage.close();
+    }
+
+    private boolean isInputValid() {
+        String errorMessage = "";
+
+        if (firstNameField.getText() == null || firstNameField.getText().length() == 0) {
+            errorMessage += "No valid first name!\n";
+        }
+
+        if (lastNameField.getText() == null || lastNameField.getText().length() == 0) {
+            errorMessage += "No valid last name!\n";
+        }
+
+        if (streetField.getText() == null || streetField.getText().length() == 0) {
+            errorMessage += "No valid street!\n";
+        }
+
+        if (cityField.getText() == null || cityField.getText().length() == 0 ) {
+            errorMessage += "No valid city!\n";
+        }
+
+        if (postalCodeField.getText() == null || postalCodeField.getText().length() == 0) {
+            errorMessage += "No valid postal code!\n";
+        } else {
+            try {
+                Integer.parseInt(postalCodeField.getText());
+            } catch (NumberFormatException e) {
+                errorMessage += "No valid postal code (must be an integer)!\n";
+            }
+        }
+
+        if (birthdayField.getText() == null || birthdayField.getText().length() == 0) {
+            errorMessage += "No valid birthday!\n";
+        } else {
+            if (!DateUtil.validDate(birthdayField.getText())) {
+                errorMessage += "No valid birthday. Use format dd.mm.yyyy!\n";
+            }
+        }
+
+        if (errorMessage.length() == 0) {
+            return true;
+        } else {
+            Dialogs.create().
+                    title("Invalid Fields")
+                    .masthead("Please correct invalid fields")
+                    .message(errorMessage)
+                    .showError();
+            return false;
+        }
+    }
 
 }
