4724aad6ee879a9472a53e62487842068e915bce
b.istomin <ibobby0386@gmail.com>
2015-08-18 11:56:15 +0300

    finish 4 lesson (black css)

--- src/ru/bobby/adress/MainApp.java
+++ src/ru/bobby/adress/MainApp.java
@@ -6,6 +6,7 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Modality;
@@ -42,6 +43,8 @@ public class MainApp extends Application {
     public void start(Stage primaryStage) throws Exception {
         this.primaryStage = primaryStage;
         this.primaryStage.setTitle("AddressApp");
+
+        this.primaryStage.getIcons().add(new Image("file:resources/images/Starbucks_Book.png"));
 
         initRootLayout();
 
